cf27b17e342c58866b00beaf12fe2786dd5f0a13
wangshaoli <1125550225@qq.com>
2021-04-30 21:29:16 +0800

    wepack import css

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,26 @@
 var path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
         filename: '[name].[contenthash].js',
-    }
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title:"my app",
+        template: "src/assets/index.html"
+
+    })],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+
 };
+
+

4bff5a58892df1196eef2283ce92df63328880d7
wangshaoli <1125550225@qq.com>
2021-05-01 15:55:46 +0800

    webpack import() 懒加载

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import x from './x.js'
 import png from './assets/1.png'
+
 console.log(x);
 console.log('hi');
 
@@ -8,3 +9,19 @@ const div = document.getElementById('app')
 div.innerHTML = `
     <img src="${png}" />
 `
+
+const btn = document.createElement('button')
+btn.innerText = '懒加载'
+btn.onclick = () => {
+    const promise = import('./lazy.js')
+    promise
+        .then((module) => {
+            const fn = module.default
+            fn()
+        })
+        .catch(() => {
+
+        })
+}
+div.appendChild(btn)
+

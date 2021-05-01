3da15d8d9246feaa8d00f319eac12f80b25229b1
wangshaoli <1125550225@qq.com>
2021-05-01 16:36:50 +0800

    update sh

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,10 @@ yarn build &&
 git checkout gh-pages &&
 rm -rf src *.sh *.html *.css *.js *.png *.json *.lock &&
 mv dist/*  ./ &&
-rm -rf dist ;
+rm -rf dist;
 
 git add . &&
 git commit -m 'update' &&
-git push &&
+git push origin gh-pages &&
 git checkout -
 
